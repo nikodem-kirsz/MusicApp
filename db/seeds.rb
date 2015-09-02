c424f04c506588ad0e88783c6aa3a549e9ff05e6
Nikodem Kirsz <nikodem.kirsz@gmail.com>
2015-09-02 13:15:02 +0200

    config

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,15 @@ def unique_name
 end
 
 
-  18.times do |n|
-       user = User.all.sample
-       user.artists.build(name: unique_name,
-                          description: "Tousled flexitarian distillery tote bag drinking vinegar, wolf vinyl synth letterpress you probably haven't heard of them tilde. Wolf DIY distillery irony Carles mumblecore. Flexitarian mumblecore four loko chia, yr meditation Pitchfork Odd Future. Pork belly trust fund banh mi bitters post-ironic, PBR jean shorts cronut listicle four dollar toast Pinterest tilde cred Intelligentsia. VHS letterpress hoodie, migas paleo selvage Banksy biodiesel roof party banjo vegan YOLO tattooed vinyl Vice. You probably haven't heard of them keffiyeh street art, trust fund cray mlkshk Austin tattooed flannel sartorial selvage. Mustache gastropub Tumblr 8-bit kale chips. Godard Pitchfork hashtag tofu PBR&B. Literally umami squid actually chillwave forage, bespoke synth whatever cardigan. Selvage DIY bicycle rights kitsch kale chips Pinterest. Mixtape try-hard Echo Park, tote bag polaroid trust fund sriracha PBR bespoke plaid banjo flexitarian four loko.", 
-                          origin: "US", 
-                          image: File.open("#{Rails.root}/app/assets/images/#{n+1}.jpg"),
-                          allow_other_to_add_songs: [true, false].sample
-                         ).save
-  end
+18.times do |n|
+     user = User.all.sample
+     user.artists.build(name: unique_name,
+                        description: "Tousled flexitarian distillery tote bag drinking vinegar, wolf vinyl synth letterpress you probably haven't heard of them tilde. Wolf DIY distillery irony Carles mumblecore. Flexitarian mumblecore four loko chia, yr meditation Pitchfork Odd Future. Pork belly trust fund banh mi bitters post-ironic, PBR jean shorts cronut listicle four dollar toast Pinterest tilde cred Intelligentsia. VHS letterpress hoodie, migas paleo selvage Banksy biodiesel roof party banjo vegan YOLO tattooed vinyl Vice. You probably haven't heard of them keffiyeh street art, trust fund cray mlkshk Austin tattooed flannel sartorial selvage. Mustache gastropub Tumblr 8-bit kale chips. Godard Pitchfork hashtag tofu PBR&B. Literally umami squid actually chillwave forage, bespoke synth whatever cardigan. Selvage DIY bicycle rights kitsch kale chips Pinterest. Mixtape try-hard Echo Park, tote bag polaroid trust fund sriracha PBR bespoke plaid banjo flexitarian four loko.", 
+                        origin: "US", 
+                        image: File.open("#{Rails.root}/app/assets/images/#{n+1}.jpg"),
+                        allow_other_to_add_songs: [true, false].sample
+                       ).save
+end
 
 40.times do |n|
      artist = Artist.all.sample
