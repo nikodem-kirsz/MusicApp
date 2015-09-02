ac968e9db95e78c2ca98b23bb6d45e07769fbf22
Nikodem Kirsz <nikodem.kirsz@gmail.com>
2015-09-02 13:00:49 +0200

    Searchbox and Amazons S3 US Storage conf finished

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,23 +8,23 @@ class Song < ActiveRecord::Base
   validates :image, :mp3, presence: true
   validates :genre, presence: true, inclusion: { in: %w(Alternative Rock Blues Electronic Folk Grunge New\ Wave Punk\ Rock Metal Pop None) }
   
-  has_attached_file :image
-                    # :storage => :s3,
-                    # :s3_credentials => {
-                    #                     :bucket => ENV['S3_BUCKET_NAME'],
-                    #                     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                    #                     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                    #                    }
+  has_attached_file :image,
+                    :storage => :s3,
+                    :s3_credentials => {
+                                        :bucket => ENV['S3_BUCKET_NAME'],
+                                        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                                        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                                       }
 
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/bmp']
 
-  has_attached_file :mp3
-                    # :storage => :s3,
-                    # :s3_credentials => {
-                    #                     :bucket => ENV['S3_BUCKET_NAME'],
-                    #                     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                    #                     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                    #                    }
+  has_attached_file :mp3,
+                    :storage => :s3,
+                    :s3_credentials => {
+                                        :bucket => ENV['S3_BUCKET_NAME'],
+                                        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                                        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                                       }
 
   validates_attachment_content_type :mp3, content_type: ['audio/mp3', 'audio/mpeg']
                     
