0160558fcf04d998e6216c1c218afc0e1da0ceb5
Nikodem Kirsz <nikodem.kirsz@gmail.com>
2015-09-02 13:39:24 +0200

    dev storage conf

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,16 +20,25 @@ end
      user.artists.build(name: unique_name,
                         description: "Tousled flexitarian distillery tote bag drinking vinegar, wolf vinyl synth letterpress you probably haven't heard of them tilde. Wolf DIY distillery irony Carles mumblecore. Flexitarian mumblecore four loko chia, yr meditation Pitchfork Odd Future. Pork belly trust fund banh mi bitters post-ironic, PBR jean shorts cronut listicle four dollar toast Pinterest tilde cred Intelligentsia. VHS letterpress hoodie, migas paleo selvage Banksy biodiesel roof party banjo vegan YOLO tattooed vinyl Vice. You probably haven't heard of them keffiyeh street art, trust fund cray mlkshk Austin tattooed flannel sartorial selvage. Mustache gastropub Tumblr 8-bit kale chips. Godard Pitchfork hashtag tofu PBR&B. Literally umami squid actually chillwave forage, bespoke synth whatever cardigan. Selvage DIY bicycle rights kitsch kale chips Pinterest. Mixtape try-hard Echo Park, tote bag polaroid trust fund sriracha PBR bespoke plaid banjo flexitarian four loko.", 
                         origin: "US", 
-                        image: File.open("#{Rails.root}/app/assets/images/#{n+1}.jpg"),
+                        image: File.open("#{Rails.root}/app/assets/images/#{rand(1..31)}.jpg"),
                         allow_other_to_add_songs: [true, false].sample
                        ).save
 end
 
 40.times do |n|
      artist = Artist.all.sample
-     artist.songs.build(image: File.open("#{Rails.root}/app/assets/images/#{rand(1..18)}.jpg"),
+     artist.songs.build(image: File.open("#{Rails.root}/app/assets/images/#{rand(1..31)}.jpg"),
                         title:  Faker::Book.title,
                         genre: SongsController::GENRES.sample,
+                        mp3: File.open("#{Rails.root}/app/assets/mp3s/#{SONGS.sample}")
+                        ).save
+end
+
+40.times do |n|
+     artist = Artist.all.sample
+     artist.songs.build(image: File.open("#{Rails.root}/app/assets/images/#{rand(1..31)}.jpg"),
+                        title:  Faker::Book.title,
+                        genre: "Alternative",
                         mp3: File.open("#{Rails.root}/app/assets/mp3s/#{SONGS.sample}")
                         ).save
 end
